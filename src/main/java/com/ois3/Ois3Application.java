fc65ef71cf4fdd2288d9d19e6103e2f7e6ce4635
eriklihhats <eriklihhats@gmail.com>
2025-01-26 17:08:36 +0200

    Initialize

---
+++ src/main/java/com/ois3/Ois3Application.java
@@ -0,0 +1,13 @@
+package com.ois3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ois3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ois3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ois3/Ois3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ois3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ois3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
